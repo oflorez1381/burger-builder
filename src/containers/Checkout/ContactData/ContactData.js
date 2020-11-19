795a0bc70488948d469b3162b1746fb3d01fef8e
oflorez1381 <oscarflorez1381@gmail.com>
2020-11-19 15:41:57 -0500

    feat: Section 12 Dynamically Create Inputs based on JS Config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
@@ -9,11 +9,63 @@ import classes from './ContactData.css';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name',
+
+                },
+                value: 'Oscar Florez'
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street',
+
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code',
+
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country',
+
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail',
+
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -24,17 +76,7 @@ class ContactData extends Component {
         this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Oscar Florez',
-                address: {
-                    street: 'Test Street',
-                    zipCode: '1111',
-                    country: 'Test'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price
         };
         axios.post('/orders.json', order)
             .then(response => {
@@ -46,19 +88,19 @@ class ContactData extends Component {
             });
     }
 
-    render(){
+    render() {
         let form = (
             <form>
-                <Input inputtype="input" type="text"  name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email"  name="email" placeholder="Your Email" />
-                <Input inputtype="input" type="text"  name="street" placeholder="Street" />
-                <Input inputtype="input" type="text"  name="postal" placeholder="Postal Code" />
+                <Input elementType="" elementConfig="" value=""/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
 
         if (this.state.loading) {
-            form = <Spinner />
+            form = <Spinner/>
         }
 
         return (
